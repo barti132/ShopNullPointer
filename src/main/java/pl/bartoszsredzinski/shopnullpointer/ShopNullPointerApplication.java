a6781f4bbd0ac80e379fc6e13981df2c8169b228
barti132 <barteksredzinski@gmail.com>
2022-12-27 01:43:05 +0100

    Initial commit

---
+++ src/main/java/pl/bartoszsredzinski/shopnullpointer/ShopNullPointerApplication.java
@@ -0,0 +1,13 @@
+package pl.bartoszsredzinski.shopnullpointer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopNullPointerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShopNullPointerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/bartoszsredzinski/shopnullpointer/ShopNullPointerApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.bartoszsredzinski.shopnullpointer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopNullPointerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
